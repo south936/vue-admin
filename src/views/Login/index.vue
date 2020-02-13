41bc3ff605c882aed92b55562541091907340c1c
unknown <you@example.com>
2020-02-13 15:14:04 +0800

    构建项目,配置全局样式,路由,elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    <el-button type="primary" plain>主要按钮</el-button>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
